f9f8c6b19b9508458a406d46aa3a8f145026a196
Sumaiyashe <sumaiyashompa137@gmail.com>
2022-02-27 23:05:13 +0600

    withdraw field

--- js/banking-site.js
+++ js/banking-site.js
@@ -5,11 +5,25 @@ document.getElementById("deposit-button").addEventListener('click', function () 
     const depositText = depositInput.value;
     // const depositValue = parseFloat(depositText);
     // get current deposit value
-    const depositeTotal = document.getElementById('deposit-total');
-    const depositTotalText = depositeTotal.innerText;
-    depositeTotal.innerText = depositText;
+    const depositTotal = document.getElementById('deposit-total');
+    const depositTotalText = depositTotal.innerText;
+    depositTotal.innerText = depositText;
     // const depositeTotalValue = parseFloat(depositeTotalText);
     console.log(depositTotalText);
     // clear input field
     depositInput.value = '';
+})
+document.getElementById("withdraw-button").addEventListener('click', function () {
+    // get withdraw-input value
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawText = withdrawInput.value;
+    // const depositValue = parseFloat(depositText);
+    // get current deposit value
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const withdrawTotalText = withdrawTotal.innerText;
+    withdrawTotal.innerText = withdrawText;
+    // const depositeTotalValue = parseFloat(depositeTotalText);
+    console.log(withdrawTotalText);
+    // clear input field
+    withdrawInput.value = '';
 })
