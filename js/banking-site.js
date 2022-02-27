48d54b676dabe8774087649ed1248ae7da2517f6
Sumaiyashe <sumaiyashompa137@gmail.com>
2022-02-28 01:55:07 +0600

    commited

--- js/banking-site.js
+++ js/banking-site.js
@@ -1,29 +1,47 @@
 // calculation side
+
 document.getElementById("deposit-button").addEventListener('click', function () {
     // get deposit-input value
     const depositInput = document.getElementById('deposit-input');
-    const depositText = depositInput.value;
-    // const depositValue = parseFloat(depositText);
+    const depositInputText = depositInput.value;
+    const depositValue = parseFloat(depositInputText);
+
     // get current deposit value
-    const depositTotal = document.getElementById('deposit-total');
+    const depositTotal = document.getElementById("deposit-total");
     const depositTotalText = depositTotal.innerText;
-    depositTotal.innerText = depositText;
-    // const depositeTotalValue = parseFloat(depositeTotalText);
-    console.log(depositTotalText);
+    const previoustTotalDeposit = parseFloat(depositTotalText);
+    depositTotal.innerText = depositValue + previoustTotalDeposit;
+
     // clear input field
     depositInput.value = '';
-})
+
+    // total balance uptade
+    const totalBalance = document.getElementById("total-balance");
+    const totalBalanceText = totalBalance.innerText;
+    const previousTotalBalance = parseFloat(totalBalanceText);
+    totalBalance.innerText = previousTotalBalance + depositValue;
+
+});
+
+
 document.getElementById("withdraw-button").addEventListener('click', function () {
     // get withdraw-input value
     const withdrawInput = document.getElementById('withdraw-input');
     const withdrawText = withdrawInput.value;
-    // const depositValue = parseFloat(depositText);
+    const withdrawValue = parseFloat(withdrawText);
     // get current deposit value
     const withdrawTotal = document.getElementById('withdraw-total');
     const withdrawTotalText = withdrawTotal.innerText;
-    withdrawTotal.innerText = withdrawText;
-    // const depositeTotalValue = parseFloat(depositeTotalText);
-    console.log(withdrawTotalText);
+    const previoustWithdrawTotalValue = parseFloat(withdrawTotalText);
+    withdrawTotal.innerText = previoustWithdrawTotalValue + withdrawValue;
+
     // clear input field
     withdrawInput.value = '';
-})
+
+    // total balance uptade
+    const totalBalance = document.getElementById("total-balance");
+    const totalBalanceText = totalBalance.innerText;
+    const previousTotalBalance = parseFloat(totalBalanceText);
+    totalBalance.innerText = previousTotalBalance - withdrawValue;
+
+});
